97bea5bd3c4385e2e97a7887643f282902e37d86
brandonclark314 <54722084+brandonclark314@users.noreply.github.com>
2022-06-08 17:49:32 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+import argparse
+import multiprocessing
+#import argparge
+import torch
+import models
+
+def getopt():
+    parser = argparse.ArgumentParser()
+
+    opt = parser.parse_args()
+    opt.kernels = multiprocessing.cpu_count()
+
+    opt.mp16folder = "/squash/MP-16-zstd/resources/images/mp16/"
+    opt.im2gps3k = "/home/al209167/datasets/im2gps3ktest/"
+
+    opt.resources = "/home/br087771/DEGeo/"
+
+    opt.size = 224
+
+    opt.n_epochs = 20
+
+    opt.description = 'ResNet50-4.2M-Im2GPS3k-softH'
+    opt.archname = 'JustResNet50'
+    opt.evaluate = False
+
+    opt.lr = 1e-2
+    opt.step_size = 3
+
+    opt.batch_size = 150
+    opt.distances = [2500, 750, 200, 25, 1]
+    opt.trainset = 'train'
+    opt.testset = 'im2gps3k'
+    opt.device = torch.device('cuda')
+
+    return opt
