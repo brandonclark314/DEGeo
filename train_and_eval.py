56db3cc7bdbc00c5ea08727ce2b8690302afab42
VicenteVivan <vicente.vivancoc@gmail.com>
2022-07-03 17:14:29 -0400

    Discretize Exponentially

--- train_and_eval.py
+++ train_and_eval.py
@@ -61,7 +61,8 @@ def train_images(train_dataloader, model, criterion, optimizer, scheduler, opt, 
         # Get Targets (GPS Cosine Similarities)
         gps_n = gps / gps.norm(dim=1, keepdim=True)
         targets = (gps_n @ gps_n.t()).float()
-        targets = discretize(targets, 1 - 0.1 * np.exp(-epoch/2))
+        targets = discretize(targets.detach().cpu().numpy(), 1 - 0.1 * np.exp(-epoch/2))
+        targets = torch.from_numpy(targets).to(opt.device)
 
         torch.set_printoptions(edgeitems=30)
 
