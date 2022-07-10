3d7f779c04ca201b0ec5e4c83d9cbd577005eb84
VicenteVivan <vicente.vivancoc@gmail.com>
2022-07-10 01:10:37 -0400

    Batch Norm

--- models.py
+++ models.py
@@ -6,7 +6,6 @@ import torch.nn.functional as F
 from torch import logit, nn
 import matplotlib.pyplot as plt
 from rff.layers import GaussianEncoding
-from siren.siren_pytorch import SirenNet
 
 class GeoCLIP(nn.Module):
     def __init__(self,  input_resolution=224):
@@ -28,7 +27,7 @@ class GeoCLIP(nn.Module):
         self.location_encoder1 = nn.Sequential(self.rff_encoding1,
                                               nn.Linear(512, 1024),
                                               nn.BatchNorm1d(1024),
-                                              nn.ReLU()
+                                              nn.ReLU(),
                                               nn.Linear(1024, 1024),
                                               nn.BatchNorm1d(1024),
                                               nn.ReLU(),
