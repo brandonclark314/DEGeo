dbc754662765d0e39c1dbd574a0385e53926542e
VicenteVivan <vicente.vivancoc@gmail.com>
2022-07-14 00:46:43 -0400

    14 Encoding

--- models.py
+++ models.py
@@ -7,94 +7,43 @@ from torch import logit, nn
 import matplotlib.pyplot as plt
 from rff.layers import GaussianEncoding
 
+def getLocationEncoder(a):
+    Earth_Diameter = 12742
+    sigma = Earth_Diameter / (3 * Earth_Diameter / 2**a)
+    rff_encoding = GaussianEncoding(sigma=sigma, input_size=3, encoded_size=256)
+    return nn.Sequential(rff_encoding,
+                         nn.Linear(512, 1024),
+                         nn.ReLU(),
+                         nn.Linear(1024, 1024),
+                         nn.ReLU(),
+                         nn.Linear(1024, 1024),
+                         nn.ReLU(),
+                         nn.Linear(1024, 512))
+
 class GeoCLIP(nn.Module):
     def __init__(self,  input_resolution=224):
         super().__init__()
 
         self.L2 = nn.functional.normalize
         self.GPS_Aug_Multiplier = 8
-        self.Earth_Diameter = 12742 # km
-        
-        # Sigma Values (1km, 200km, 2500km)
-        sigma1 = self.Earth_Diameter / (3 * 0.25)
-        sigma2 = self.Earth_Diameter / (3 * 0.5)
-        sigma3 = self.Earth_Diameter / (3 * 1)
-        sigma4 = self.Earth_Diameter / (3 * 25)
-        sigma5 = self.Earth_Diameter / (3 * 200)
-        sigma6 = self.Earth_Diameter / (3 * 750)
-        sigma7 = self.Earth_Diameter / (3 * 2500) 
         
         self.image_encoder = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k", output_hidden_states=True)
-        self.rff_encoding1 = GaussianEncoding(sigma=sigma1, input_size=3, encoded_size=256)
-        self.rff_encoding2 = GaussianEncoding(sigma=sigma2, input_size=3, encoded_size=256)
-        self.rff_encoding3 = GaussianEncoding(sigma=sigma3, input_size=3, encoded_size=256)
-        self.rff_encoding4 = GaussianEncoding(sigma=sigma4, input_size=3, encoded_size=256)
-        self.rff_encoding5 = GaussianEncoding(sigma=sigma5, input_size=3, encoded_size=256)
-        self.rff_encoding6 = GaussianEncoding(sigma=sigma6, input_size=3, encoded_size=256)
-        self.rff_encoding7 = GaussianEncoding(sigma=sigma7, input_size=3, encoded_size=256)
-        
-        self.location_encoder1 = nn.Sequential(self.rff_encoding1,
-                                                nn.Linear(512, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 512))
-        
-        self.location_encoder2 = nn.Sequential(self.rff_encoding2,
-                                                nn.Linear(512, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 512))
-        
-        self.location_encoder3 = nn.Sequential(self.rff_encoding3,
-                                                nn.Linear(512, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 512))
-                                                
-        self.location_encoder4 = nn.Sequential(self.rff_encoding4,
-                                                nn.Linear(512, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 512))
-        
-        self.location_encoder5 = nn.Sequential(self.rff_encoding5,
-                                                nn.Linear(512, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 512))
 
-        self.location_encoder6 = nn.Sequential(self.rff_encoding6,
-                                                nn.Linear(512, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 512))
-        
-        self.location_encoder7 = nn.Sequential(self.rff_encoding7,
-                                                nn.Linear(512, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 1024),
-                                                nn.ReLU(),
-                                                nn.Linear(1024, 512))
+        self.location_encoder0 = getLocationEncoder(0)
+        self.location_encoder1 = getLocationEncoder(1)
+        self.location_encoder2 = getLocationEncoder(2)
+        self.location_encoder3 = getLocationEncoder(3)
+        self.location_encoder4 = getLocationEncoder(4)
+        self.location_encoder5 = getLocationEncoder(5)
+        self.location_encoder6 = getLocationEncoder(6)
+        self.location_encoder7 = getLocationEncoder(7)
+        self.location_encoder8 = getLocationEncoder(8)
+        self.location_encoder9 = getLocationEncoder(9)
+        self.location_encoder10 = getLocationEncoder(10)
+        self.location_encoder11 = getLocationEncoder(11)
+        self.location_encoder12 = getLocationEncoder(12)
+        self.location_encoder13 = getLocationEncoder(13)
+        self.location_encoder14 = getLocationEncoder(14)
 
         self.mlp = nn.Sequential(nn.Linear(768, 512))
         self.input_resolution = input_resolution
@@ -111,7 +60,14 @@ class GeoCLIP(nn.Module):
                 self.location_encoder4(location),
                 self.location_encoder5(location),
                 self.location_encoder6(location),
-                self.location_encoder7(location)]
+                self.location_encoder7(location),
+                self.location_encoder8(location),
+                self.location_encoder9(location),
+                self.location_encoder10(location),
+                self.location_encoder11(location),
+                self.location_encoder12(location),
+                self.location_encoder13(location),
+                self.location_encoder14(location)]
                                              
     def forward(self, image, location):
         image_features = self.encode_image(image).last_hidden_state
@@ -132,6 +88,13 @@ class GeoCLIP(nn.Module):
         location_features5 = location_features5 / location_features5.norm(dim=1, keepdim=True)
         location_features6 = location_features6 / location_features6.norm(dim=1, keepdim=True)
         location_features7 = location_features7 / location_features7.norm(dim=1, keepdim=True)
+        location_features8 = location_features8 / location_features8.norm(dim=1, keepdim=True)
+        location_features9 = location_features9 / location_features9.norm(dim=1, keepdim=True)
+        location_features10 = location_features10 / location_features10.norm(dim=1, keepdim=True)
+        location_features11 = location_features11 / location_features11.norm(dim=1, keepdim=True)
+        location_features12 = location_features12 / location_features12.norm(dim=1, keepdim=True)
+        location_features13 = location_features13 / location_features13.norm(dim=1, keepdim=True)
+        location_features14 = location_features14 / location_features14.norm(dim=1, keepdim=True)
 
         # Cosine similarity as logits
         logit_scale = self.logit_scale.exp()
@@ -146,6 +109,13 @@ class GeoCLIP(nn.Module):
         p5 = s(image_features @ location_features5.t())
         p6 = s(image_features @ location_features6.t())
         p7 = s(image_features @ location_features7.t())
+        p8 = s(image_features @ location_features8.t())
+        p9 = s(image_features @ location_features9.t())
+        p10 = s(image_features @ location_features10.t())
+        p11 = s(image_features @ location_features11.t())
+        p12 = s(image_features @ location_features12.t())
+        p13 = s(image_features @ location_features13.t())
+        p14 = s(image_features @ location_features14.t())
         
         P = 1 / (1 + (1 / p1 - 1) * \
                      (1 / p2 - 1) * \
@@ -153,7 +123,14 @@ class GeoCLIP(nn.Module):
                      (1 / p4 - 1) * \
                      (1 / p5 - 1) * \
                      (1 / p6 - 1) * \
-                     (1 / p7 - 1))
+                     (1 / p7 - 1) * \
+                     (1 / p8 - 1) * \
+                     (1 / p9 - 1) * \
+                     (1 / p10 - 1) * \
+                     (1 / p11 - 1) * \
+                     (1 / p12 - 1) * \
+                     (1 / p13 - 1) * \
+                     (1 / p14 - 1))
         
         logits_per_image = logit_scale * P
           
