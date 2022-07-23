95e47699476ca493f407fcf23d145fefb44e801c
VicenteVivan <vicente.vivancoc@gmail.com>
2022-07-22 21:00:45 -0400

    100 Steps Eval

--- config.py
+++ config.py
@@ -15,12 +15,12 @@ def getopt():
 
     opt.resources = "/home/br087771/DEGeo/"
 
-    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K (RFF)/GeoCLIP100K SGD Eval.pth"
+    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K (RFF)/GeoCLIP100K 100 SGD Eval.pth"
 
     opt.size = 224
     opt.n_epochs = 64
 
-    opt.description = 'GeoCLIP100K SGD Eval'
+    opt.description = 'GeoCLIP100K 100 SGD Eval'
     opt.archname = 'CLIP'
     opt.evaluate = False
     opt.scene = False
