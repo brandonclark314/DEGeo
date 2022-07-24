065b8f5d63f90aa59d5701a5cd7fb5d62683ce25
VicenteVivan <vicente.vivancoc@gmail.com>
2022-07-24 17:22:48 -0400

    Batch Size Multiplier

--- config.py
+++ config.py
@@ -15,23 +15,23 @@ def getopt():
 
     opt.resources = "/home/br087771/DEGeo/"
 
-    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K (RFF)/GeoCLIP100K 100k Adam.pth"
+    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K (RFF)/GeoCLIP100K SGD 256 100k.pth"
 
     opt.size = 224
     opt.n_epochs = 64
 
-    opt.description = 'GeoCLIP100K Momentum x100 Adam'
+    opt.description = 'GeoCLIP100K Momentum SGD (256) x100'
     opt.archname = 'CLIP'
     opt.evaluate = False
     opt.scene = False
     opt.hier_eval = True
 
-    opt.lr = 3e-5
+    opt.lr = 1e-2
     opt.step_size = 3
     opt.partition = 'fine'
 
-    opt.queue_bs_multiplier = 100
-    opt.batch_size = 32
+    opt.queue_bs_multiplier = 256
+    opt.batch_size = 256
     opt.distances = [2500, 750, 200, 25, 1]
     opt.trainset = 'train100K'
     opt.testset = 'im2gps3k'
