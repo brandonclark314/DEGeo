5a9f272b5fe653bcf616e19c6ad9433d7d35e441
VicenteVivan <vicente.vivancoc@gmail.com>
2022-07-30 20:55:08 -0400

    Print Debug

--- gaussian.py
+++ gaussian.py
@@ -80,13 +80,15 @@ def gaussian_eval(val_dataloader, model, epoch, opt):
         locations_opt = locations_opt[torch.flatten(top_k_i)]
         
         for distance in [100, 50, 25, 10, 5, 1, 0.5, 0.25, 0.1]:
-            print("Locations: ", locations_opt.shape, flush=True)
-            locations_opt = zoom_in(locations_opt, n=25, km_std=distance)
+            print("\n Locations: ", locations_opt.shape, flush=True)
+            locations_opt = zoom_in(locations_opt, n=15, km_std=distance)
             logits_per_image, logits_per_location, scene_pred, \
                 img_momentum_matrix, gps_momentum_matrix = model(imgs, locations_opt)
             probs = logits_per_image.softmax(dim=-1)
             top_k, top_k_i = torch.topk(probs, k=10, dim=-1)
             locations_opt = locations_opt[torch.flatten(top_k_i)]
+            
+            print("\n Top 10: ", top_k, flush=True)
         
         # Last Prediction
         logits_per_image, logits_per_location, scene_pred, \
