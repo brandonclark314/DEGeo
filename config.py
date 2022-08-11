94b5f1c57f4b960bb394ba170302d0aae48aece1
VicenteVivan <vicente.vivancoc@gmail.com>
2022-08-11 16:52:08 -0400

    Small LR

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ def getopt():
     opt.hier_eval = False
 
     # opt.lr = 3e-5
-    # opt.lr = 5e-4
-    opt.lr = 0.03
+    opt.lr = 5e-4
+    # opt.lr = 0.03
     opt.step_size = 3
     opt.partition = 'fine'
 
