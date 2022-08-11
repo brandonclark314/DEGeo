6745dbe14506ca2f181b379584a33a3b3dcd7a38
VicenteVivan <vicente.vivancoc@gmail.com>
2022-08-11 16:27:49 -0400

    RAdam

--- config.py
+++ config.py
@@ -16,24 +16,25 @@ def getopt():
 
     opt.resources = "/home/br087771/DEGeo/"
 
-    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K SGD AutoencoderV.pth"
+    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K RAdam (256).pth"
     # opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K Adam (32) x 100 768Dim.pth"
 
     opt.size = 224
     opt.n_epochs = 64
 
-    opt.description = 'GeoCLIP100K SGD AutoencoderV'
+    opt.description = 'GeoCLIP100K RAdam (256)'
     opt.archname = 'CLIP'
     opt.evaluate = False
     opt.scene = False
     opt.hier_eval = False
 
     # opt.lr = 3e-5
-    opt.lr = 0.01
+    opt.lr = 5e-4
+    # opt.lr = 0.01
     opt.step_size = 3
     opt.partition = 'fine'
 
-    opt.batch_size = 32
+    opt.batch_size = 256
     opt.distances = [2500, 750, 200, 25, 1]
     opt.trainset = 'train100K'
     opt.testset = 'im2gps3k'
