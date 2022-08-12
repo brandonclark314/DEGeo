5e94c96186c8a969a9f99a3cbff9fe8e9f06a560
VicenteVivan <vicente.vivancoc@gmail.com>
2022-08-11 23:31:07 -0400

    Small Momentum

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ def getopt():
 
     opt.resources = "/home/br087771/DEGeo/"
 
-    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K RAdam (256) Q32K.pth"
+    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K RAdam .9999 (256) Q32K.pth"
     # opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K Adam (32) x 100 768Dim.pth"
 
     opt.size = 224
     opt.n_epochs = 64
 
-    opt.description = 'GeoCLIP100K RAdam (256) Q32K'
+    opt.description = 'GeoCLIP100K RAdam (256) .9999 Q32K'
     opt.archname = 'CLIP'
     opt.evaluate = False
     opt.scene = False
