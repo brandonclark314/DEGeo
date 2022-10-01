9d66feb6957992cff7389d3fc50a6bdfc2012cc4
Vicente Vivanco Cepeda <vicentevivanco@knights.ucf.edu>
2022-10-01 14:06:23 -0400

    1024D

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ def getopt():
 
     opt.resources = "/home/br087771/DEGeo/"
 
-    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K Adam-NS (32) Focal Loss Q4K 768D Grad LatLon.pth"
+    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K Adam-NS (32) Focal Loss Q4K 1024D Grad LatLon.pth"
     # opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K Adam (32) x 100 768Dim.pth"
 
     opt.size = 224
     opt.n_epochs = 64
 
-    opt.description = 'GeoCLIP100K Adam-NS (32) Focal Loss Q4K 768D Grad LatLon'
+    opt.description = 'GeoCLIP100K Adam-NS (32) Focal Loss Q4K 1024D Grad LatLon'
     opt.archname = 'CLIP'
     opt.evaluate = False
     opt.scene = False
