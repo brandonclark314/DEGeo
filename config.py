13e1b4445f80f14e472dcbe5619eb25772c5b8af
Vicente Vivanco Cepeda <vicentevivanco@knights.ucf.edu>
2022-10-09 14:17:04 -0400

    Queue 32K

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ def getopt():
 
     opt.resources = "/home/br087771/DEGeo/"
 
-    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP4M Adam (256) ResMLP.pth"
+    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP4M Adam (256) ResMLP Q32K.pth"
     # opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP4M Adam (256) Focal Loss Q4K 768D Grad LatLon_3_4.87.pth"
 
     opt.size = 224
     opt.n_epochs = 64
 
-    opt.description = 'GeoCLIP4M Adam (256) ResMLP'
+    opt.description = 'GeoCLIP4M Adam (256) ResMLP Q32K'
     opt.archname = 'CLIP'
     opt.evaluate = False
     opt.scene = False 
@@ -34,8 +34,8 @@ def getopt():
     opt.step_size = 3
     opt.partition = 'fine'
 
-    opt.queue_size = 4096
-    # opt.queue_size = 32768
+    # opt.queue_size = 4096
+    opt.queue_size = 32768
     opt.batch_size = 256
     opt.distances = [2500, 750, 200, 25, 1]
     opt.trainset = 'train'
