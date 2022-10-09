2acb6954e826c77016bbacaf3483f1f0fab499c9
Vicente Vivanco Cepeda <vicentevivanco@knights.ucf.edu>
2022-10-09 13:57:21 -0400

    ResMLP 4M

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ def getopt():
 
     opt.resources = "/home/br087771/DEGeo/"
 
-    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP100K Adam (256) Q8K.pth"
+    opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP4M Adam (256) ResMLP.pth"
     # opt.saved_model = "/home/vi844593/DEGeo/weights/GeoCLIP4M Adam (256) Focal Loss Q4K 768D Grad LatLon_3_4.87.pth"
 
     opt.size = 224
     opt.n_epochs = 64
 
-    opt.description = 'GeoCLIP100K Adam (256) Q8K'
+    opt.description = 'GeoCLIP4M Adam (256) ResMLP'
     opt.archname = 'CLIP'
     opt.evaluate = False
     opt.scene = False 
@@ -34,12 +34,12 @@ def getopt():
     opt.step_size = 3
     opt.partition = 'fine'
 
-    # opt.queue_size = 4096
-    opt.queue_size = 8192
+    opt.queue_size = 4096
+    # opt.queue_size = 32768
     opt.batch_size = 256
     opt.distances = [2500, 750, 200, 25, 1]
-    opt.trainset = 'train100K'
-    opt.testset = 'yfcc26k'
+    opt.trainset = 'train'
+    opt.testset = 'im2gps3k'
     opt.traintype = 'CLIP'
     opt.device = torch.device('cuda')
 
--- models.py
+++ models.py
@@ -50,33 +50,6 @@ class ResMLPBlock(nn.Module):
         x = nn.ReLU()(x)
         return x
 
-# class LocationEncoderCapsule(nn.Module):
-#     def __init__(self, km):
-#         super(LocationEncoderCapsule, self).__init__()
-#         Earth_Diameter = 12742
-#         sigma = Earth_Diameter / (3 * km)
-#         rff_encoding = GaussianEncoding(sigma=sigma, input_size=2, encoded_size=256)
-#         self.km = km
-
-#         self.base = nn.Sequential(rff_encoding,
-#                                   nn.Linear(512, 1024),
-#                                   nn.ReLU(),
-#                                   nn.Linear(1024, 1024),
-#                                   nn.ReLU())
-
-#         self.capsule = nn.Sequential(ResMLPBlock(1024),
-#                                      ResMLPBlock(1024),
-#                                      ResMLPBlock(1024),
-#                                      ResMLPBlock(1024))
-
-#         self.head = nn.Sequential(nn.Linear(1024, 768))
-
-#     def forward(self, x):
-#         x = self.base(x)
-#         x = self.capsule(x)
-#         x = self.head(x)
-#         return x
-
 class LocationEncoderCapsule(nn.Module):
     def __init__(self, km):
         super(LocationEncoderCapsule, self).__init__()
@@ -85,20 +58,47 @@ class LocationEncoderCapsule(nn.Module):
         rff_encoding = GaussianEncoding(sigma=sigma, input_size=2, encoded_size=256)
         self.km = km
 
-        self.capsule = nn.Sequential(rff_encoding,
-                                     nn.Linear(512, 1024),
-                                     nn.ReLU(),
-                                     nn.Linear(1024, 1024),
-                                     nn.ReLU(),
-                                     nn.Linear(1024, 1024),
-                                     nn.ReLU())
+        self.base = nn.Sequential(rff_encoding,
+                                  nn.Linear(512, 1024),
+                                  nn.ReLU(),
+                                  nn.Linear(1024, 1024),
+                                  nn.ReLU())
+
+        self.capsule = nn.Sequential(ResMLPBlock(1024),
+                                     ResMLPBlock(1024),
+                                     ResMLPBlock(1024),
+                                     ResMLPBlock(1024))
 
         self.head = nn.Sequential(nn.Linear(1024, 768))
 
     def forward(self, x):
+        x = self.base(x)
         x = self.capsule(x)
         x = self.head(x)
         return x
+
+# class LocationEncoderCapsule(nn.Module):
+#     def __init__(self, km):
+#         super(LocationEncoderCapsule, self).__init__()
+#         Earth_Diameter = 12742
+#         sigma = Earth_Diameter / (3 * km)
+#         rff_encoding = GaussianEncoding(sigma=sigma, input_size=2, encoded_size=256)
+#         self.km = km
+
+#         self.capsule = nn.Sequential(rff_encoding,
+#                                      nn.Linear(512, 1024),
+#                                      nn.ReLU(),
+#                                      nn.Linear(1024, 1024),
+#                                      nn.ReLU(),
+#                                      nn.Linear(1024, 1024),
+#                                      nn.ReLU())
+
+#         self.head = nn.Sequential(nn.Linear(1024, 768))
+
+#     def forward(self, x):
+#         x = self.capsule(x)
+#         x = self.head(x)
+#         return x
     
 class LocationEncoder(nn.Module):
     def __init__(self, opt=None):
